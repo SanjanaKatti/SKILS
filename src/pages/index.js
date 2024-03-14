fb48368eb85f51ceb725cd4f4928b5787aacc32f
SanjanaKatti <sanjanakatti16@gmail.com>
2024-03-14 14:55:20 +0530

    skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,66 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+function Home(){
+    return(
+      <div>
+        <div className="flex flex-row justify-between items-center py-30 border-2">
+          <div classname="w-1/2"><img className="w-299 h-155" src="https://skilshruti.com/wp-content/uploads/2020/01/skils-LOGO_compressed-1.png"/></div>
+          <div className="flex w-1/2">
+              <div className="px-4">Home</div>
+              <div className="px-4">About</div>
+              <div className="px-4">Productions</div>
+              <div className="px-4">Other Works</div>
+              <div className="px-4">Gallery</div>
+              <div className="px-4">FAQs</div>
+              <div className="px-4">Contact Us</div>
+          </div>
+        </div>
+      <div className="h-96 flex justify-center items-end border-2">
+        <div className="flex flex-col justify-center items-center">
+            <h1>SHRUTI PATKI</h1>
+            <h3>KATHAK EXPONENT</h3>
+            <div className="flex flew-row justify-between items-center">
+              <div className="px-2">FB</div>
+              <div className="px-2">Insta</div>
+              <div className="px-2">YT</div>
+            </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="h-96 flex flex-col justify-center items-center border-2">
+        <div>Abbreviation</div>
+        <div>Content</div>
+        <div>Button</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="h-96 flex flex-col justify-center items-center border-2">
+        <div>Online classes</div>
+        <div>Stage Performance</div>
+        <div>Professional Training</div>
       </div>
-    </main>
-  );
+      <div className="h-96 flex flex-col items-center border-2">
+        <div><h1 className="text-4xl">Gallery</h1></div>
+        <div><h3>Images</h3></div>
+      </div>
+      <div className="h-96 flex flex-row justify-between items-center border-2">
+        <div className="h-1/3 w-1/5 flex justify-center items-center border p-5">Students</div>
+        <div className="h-1/3 w-1/5 flex justify-center items-center border p-5">Branches</div>
+        <div className="h-1/3 w-1/5 flex justify-center items-center border p-5">Performances</div>
+        <div className="h-1/3 w-1/5 flex justify-center items-center border p-5">Awards</div>
+      </div>
+      <div className="h-24 flex flex-row justify-between items-center border-2">
+        <div className="text-2xl">Please Follow and Subscribe</div>
+        <div className="flex flex-row items-end">
+          <div className="px-2">FB</div>
+          <div className="px-2">Insta</div>
+          <div className="px-2">Youtube</div>
+        </div>
+      </div>
+      <div className="h-96 flex flex-row justify-center border-2">
+        <div>Form</div>
+      </div>
+      <div className="h-24 flex flex-row justify-center border-2">
+        Footer
+      </div>
+    </div>
+    );
 }
+
+export default Home;
